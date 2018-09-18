284247850836f28eb3f836d491f5c03291803237
figures <itstepbackdor@outlook.com>
2018-09-18 18:46:02 +0300

    Add project files.

---
+++ figures_hw/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+//Описать несколько классов геометрических фигур.У каждой сделать свойство или метод расчёта её площади.
+//Создать по несколько экземпляров каждого объекта.Все эти объекты поместить в одну коллекцию.
+//И посчитать суммарную площадь всех фигур.
+
+
+namespace ConsoleApp5
+{
+    class Program// я плохо знаю англ язык, по этому некоторые слова для себя перевожу на русс.
+    {
+        static void Main(string[] args)
+        {
+            List<figures> collection = new List<figures>();
+            var rectangle = new rectangle(5, 7);
+            var right_triangle = new right_triangle(10, 4);
+            collection.Add(rectangle);
+            collection.Add(right_triangle);
+            int sum_area = 0;
+            foreach (var item in collection)
+            {
+                sum_area += item.value_area();
+            }
+            Console.WriteLine(sum_area);
+        }
+    }
+}
---
+++ figures_hw/figures/rectangle.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp5
+{
+    class rectangle : figures//прямоугольник
+    {
+        int vertical_side;
+        int horizontal_side;
+        public rectangle(int vertical, int horizontal)
+        {
+            vertical_side = vertical;
+            horizontal_side = horizontal;
+        }
+        public int value_area()
+        {
+            if (vertical_side > 0 && horizontal_side > 0)
+                return vertical_side * horizontal_side;
+            else return 0;
+        }
+    }
+}
